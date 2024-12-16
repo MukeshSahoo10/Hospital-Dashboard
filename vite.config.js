dbdd8bc3c8d3408a8c76715c1926c1cd39a7a5fe
Mukesh Sahoo <118727823+MukeshSahoo10@users.noreply.github.com>
2024-12-17 00:03:17 +0530

    second commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/dashboard/',  // This is important for GitHub Pages or sub-path deployments
+});
