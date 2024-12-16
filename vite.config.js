31ea9b9b8f1ea6d3a63463c57fc95d21845c85ee
Mukesh Sahoo <118727823+MukeshSahoo10@users.noreply.github.com>
2024-12-17 00:19:05 +0530

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/dashboard/',  // This is important for GitHub Pages or sub-path deployments
+  base: '/Hospital-Dashboard/',  // This is important for GitHub Pages or sub-path deployments
 });
